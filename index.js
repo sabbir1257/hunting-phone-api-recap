a12c85fff19cbc14cc7d5962177acf0c8be17e11
sabbir1257 <masabbir1257@gmail.com>
2024-11-23 16:04:33 +0600

    show all button sction push

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
-const lodingAPI = async (searchText) => {
+const lodingAPI = async (searchText, isShowAll) => {
      const res = await fetch(` https://openapi.programming-hero.com/api/phones?search=${searchText}`);
      const data = await res.json();
-     showCardData(data.data);
+     const phones = data.data
+     showCardData(phones, isShowAll);
 };
 
-const handleSearch  = () => {
+const handleSearch = (isShowAll) => {
      const searchInput = document.getElementById('search-input');
      const searchText = searchInput.value;
      // console.log(value)
-     lodingAPI(searchText);
+     lodingAPI(searchText, isShowAll);
 }
 
-const showCardData = (data) => {
+const showCardData = (phones, isShowAll) => {
      // console.log(data);
-
      const container = document.getElementById('show-card-container');
      container.textContent = '';
 
-     for (let value of data) {
-          console.log(value)
+
+     // show all container 
+     const showContainer = document.getElementById('show-all-container');
+     if (phones.length > 9 && !isShowAll) {
+          showContainer.classList.remove('hidden');
+     }
+     else {
+          showContainer.classList.add('hidden');
+     }
+
+     // console.log('is show all', isShowAll)
+
+     if (!isShowAll) {
+          phones = phones.slice(0, 9)
+     }
+
+     for (let value of phones) {
+          // console.log(value)
           const div = document.createElement("div");
           div.innerHTML = `
                <div class="w-4/6 border border-[#CFCFCF]  mx-auto grid justify-items-center text-center gap-4 p-5 rounded-md">
@@ -33,3 +49,8 @@ const showCardData = (data) => {
      };
 
 };
+
+const handleshow = () => {
+     handleSearch(true);
+}
+
